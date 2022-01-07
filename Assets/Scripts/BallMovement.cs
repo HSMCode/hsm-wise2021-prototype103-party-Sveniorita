3fe7baabe64e3e49ba8e6acd7496764d13b5dd41
Sveniorita <92942919+Sveniorita@users.noreply.github.com>
2022-01-07 16:46:13 +0100

    Ball Physics usw.

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -6,18 +6,28 @@ public class BallMovement : MonoBehaviour
 {
 
     public float speed;
+   // public float time = 5f;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //invoke("Destroy", time);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+
+
     }
 
- 
+
+    public void Destroy()
+    {
+       // destroy.gameObject();
+    }
 }
